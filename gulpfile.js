4cd5c6fa67a6d887adc3140eb2cfba51a43c51b9
M1RHRV <mir.hrv3@gmail.com>
2025-04-09 23:57:58 -0400

    Update gulpfile.js
    Added more tasks for dev and prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,65 @@
-const {src, dest, series, watch} = require('gulp');
-    htmlValidator = require('gulp-html');
-    htmlCompressor = require('gulp-htmlmin');
-    cssValidator = require(`gulp-stylelint`),
-    cssCompressor = require(`gulp-clean-css`);
-    jsLinter = require(`gulp-eslint`);
-    jsCompressor = require(`gulp-uglify`);
-    babel = require(`gulp-babel`);
+const { src, dest, series, watch } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const cssValidator = require(`gulp-stylelint`);
+const cssCompressor = require(`gulp-clean-css`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
+const babel = require(`gulp-babel`);
 
+let validateHTML = () => {
+    return src(`index.html`)
+        .pipe(htmlValidator())
+        .pipe(dest(`dist`));
+};
 
-    let ValidateHtml = ()=> {
-        return src('index.html')
-            .pipe(htmlValidator())
-            .pipe(dest('dist'));
-    }
+let compressHTML = () => {
+    return src(`index.html`)
+        .pipe(htmlCompressor({
+            collapseWhitespace: true,
+        }))
+        .pipe(dest(`production`));
+};
+let validateJS = () => {
+    return src(`scripts/main.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
 
-    let CompressHtml = () => {
-        return src('index.html')
-            .pipe(htmlCompressor({
-                collapseWhitespace: true,
-            }))
-            .pipe(dest('production'));
-    }
+let validateCSS = () => {
+    return src(`styles/*.css`)
+        .pipe(cssValidator({
+            failAfterError: false,
+            reporters: [{
+                formatter: `string`,
+                console: true
+            }]
+        }));
+};
+
+let compressCSS = () => {
+    return src(`styles/*.css`)
+        .pipe(cssCompressor())
+        .pipe(dest(`prod/styles`));
+};
+
+let transpileJSForDev = () => {
+    return src(`scripts/main.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+
+let transpileJSForProd = () => {
+    return src(`scripts/main.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.validateCSS = validateCSS;
+exports.compressCSS = compressCSS;
+exports.validateJS = validateJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
